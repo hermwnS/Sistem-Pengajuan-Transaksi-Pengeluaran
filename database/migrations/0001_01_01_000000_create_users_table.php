d2a5cd9e2d1bb02dc57d782fdb94e5dfe7302953
Hermawan Saputra <126874286+hermwnS@users.noreply.github.com>
2026-07-08 11:44:56 +0700

    memperbaiki migration tabel user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id_users();
+            $table->id_user();
             $table->string('name');
             $table->string('email');
             $table->foreignId('id_category')->constrained('categories', 'id_category');
